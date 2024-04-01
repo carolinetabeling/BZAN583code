f95a1dff5ee4f90752a451f7331f0088ac4f63f7
carolinetabeling <143127571+carolinetabeling@users.noreply.github.com>
2024-04-01 19:29:11 -0400

    new code

--- profile.R
+++ profile.R
@@ -1,17 +1,17 @@
 Rprof()
 start <- Sys.time()
+
 set.seed(123)
-
 ntrials <- 1000000
-strikeout <- c()
-for (trial in 1:ntrials) {
-  pitch_prob <- c(.4,.6) #ball then strike
-  pitch <- c(0,1) #ball - 0, strike - 1
-  pitcher <- sample(pitch, size = 6, replace = TRUE, prob = pitch_prob)
-  strikeout[trial] <- sum(pitcher[1:6]) >= 3
-}
-mean(strikeout) 
+pitch_prob <- c(.4,.6) #ball then strike
+pitch <- c(0,1) #ball - 0, strike - 1
+pitcher <- sample(pitch, size = 6*ntrials, replace = TRUE, prob = pitch_prob)
 
+strikeouts <- numeric(length(pitcher)/6)  
+for (i in seq(1, length(pitcher), by = 6)) {
+  strikeouts[i/6] <- sum(pitcher[i:(i+5)]) >= 3
+}
+mean(strikeouts)
 Rprof(NULL)
 summaryRprof()
 
