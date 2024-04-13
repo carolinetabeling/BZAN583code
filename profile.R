59ba528488250b128b8e6a29cebcc5cc4d99c3fc
carolinetabeling <143127571+carolinetabeling@users.noreply.github.com>
2024-04-12 20:30:20 -0400

    assignment 4

--- profile.R
+++ profile.R
@@ -1,9 +1,4 @@
-library(arrow)
-library(data.table)
-library(dplyr)
 
-weather <- data.table::fread("/projects/bckj/Team4/Data/automatic_weather_stations_inmet_brazil_2000_2021.csv", sep = ";")
-
-arrow::write_dataset(weather, "/projects/bckj/Team4/Data/parquet_data", partitioning = c("ESTACAO"))
-
-#read_parquet("/projects/bckj/Team4/Data/parquet_datapart-0.parquet")
+du -hs /projects/bckj/
+  
+du -hs /projects/bckj/Team4
